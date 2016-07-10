a8edf6172147352d8900ea720aebb3c7295a1f97
David Cruz Anaya <info@dacran.co.uk>
2016-07-10 19:42:35 +0100

    Add MenuScene and collusion sounds

---
+++ Zombie-Run/MenuScene.swift
@@ -0,0 +1,16 @@
+//
+//  MenuScene.swift
+//  Zombie-Run
+//
+//  Created by David Cruz Anaya on 10/07/2016.
+//  Copyright © 2016 David Cruz Anaya. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MenuScene: SKScene {
+    var soundToPlay: String?
+    var label: SKLabelNode?
+    
+}
